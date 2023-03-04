3308de9773ab03c071210e0825acf64aea0961f5
御姐 <162095214@qq.com>
2023-03-04 09:56:37 +0800

    add print

--- dgn/training.py
+++ dgn/training.py
@@ -21,6 +21,8 @@ from utils.ema import initializes_target_network, update_target_network_paramete
 from utils.mae import get_mask_x_dict, get_mae_loss
 from models.gat import UnnamedModel
 
+torch.set_printoptions(threshold=np.inf)
+
 Default_Hparams = {
     "train_batch_size": 128,
     "test_batch_size": 128,
@@ -100,13 +102,25 @@ def predict(model, device, loader_test, graph_data, mae=True):
             edge_index_dict = graph_data.collect('edge_index')
 
             output, _ = model(drug1_ids, drug2_ids, cell_features, x_dict, edge_index_dict)
+            print("model output: {}".format(output))
 
             ys = F.softmax(output, 1).to('cpu').data.numpy()
+            print("model output after softmax: {}".format(ys))
+
             predicted_labels = list(map(lambda x: np.argmax(x), ys))
+            print("curr predicted_labels: {}".format(predicted_labels))
+
             predicted_scores = list(map(lambda x: x[1], ys))
-            total_preds = torch.cat((total_preds, torch.Tensor(predicted_scores)), 0)
+            print("curr predicted_scores: {}".format(predicted_scores))
+
             total_prelabels = torch.cat((total_prelabels, torch.Tensor(predicted_labels)), 0)
+            print("total_prelabels: {}".format(total_preds))
+
+            total_preds = torch.cat((total_preds, torch.Tensor(predicted_scores)), 0)
+            print("total_preds: {}".format(total_preds))
+
             total_labels = torch.cat((total_labels, labels.cpu()), 0)
+            print("total_labels: {}".format(total_labels))
 
     return total_labels.numpy().flatten(), total_preds.numpy().flatten(), total_prelabels.numpy().flatten()
 
