e8bd505fdadac4b35b39dd450b696c38ed12747a
御姐 <162095214@qq.com>
2023-03-20 10:53:43 +0800

    add sh

---
+++ dgn/bin/s1f1.sh
@@ -0,0 +1,13 @@
+# usage
+# cd ~/data/DGN/dgn
+# nohup sh bin/s1f1.sh > bin/sh_log/s1f1.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=1,2,3, TORCH_DISTRIBUTED_DEBUG=DETAIL accelerate launch ./training.py \
+--setting=1 \
+--train_batch_size=512 \
+--test_batch_size=512 \
+--lr=4e-5 \
+--fold=1 \
+--log_step=20 \
+--num_workers=4 \
+>bin/train_log/setting4fold1.log
